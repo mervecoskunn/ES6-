82f7089c35c4b286e07953a4298ed54ba96f9ad0
Merve <83183081+mervecoskunn@users.noreply.github.com>
2022-12-05 10:08:00 +0100

    reference and primitive types

---
+++ Reference types/app.js
@@ -0,0 +1,17 @@
+let a ="Merve";
+let b ="Merve";
+if(a===b){
+    console.log("Same");
+
+}
+
+let array1= [1,2,3,4,5];
+let array2=[1,2,3,4,5];
+
+if(array1===array2){
+    console.log("same");
+
+}
+else{
+    console.log("not same");
+}
